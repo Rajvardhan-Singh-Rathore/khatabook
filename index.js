76ca7f895d72b4896cdaec4cfc99c7283c49ab6d
Rajvardhan Singh Rathore <rajvardhansinghrathore1990@gmail.com>
2025-10-21 10:52:15 +0530

    Configured MongoDB Atlas and ready for Render deploy

--- index.js
+++ index.js
@@ -1,80 +1,58 @@
 const express = require('express');
-const app = express();
-const fs = require('fs');
+const mongoose = require('mongoose');
 const path = require('path');
-const currentDate = new Date();
 
-//extracting file name using date
-const file_name = `${currentDate.getDate().toString()+"/"+(currentDate.getMonth()+1).toString()+"/"+currentDate.getFullYear().toString().slice(2,4)}`;
-// console.log(file_name);
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://forfarzivada_db_user:knkszt9aujD5RAgN@khatabook.kutpwvm.mongodb.net/khatabook?retryWrites=true&w=majority&appName=Khatabook')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+const recordSchema = new mongoose.Schema({
+  date: String,
+  details: String
+});
+const Record = mongoose.model('Record', recordSchema);
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const callback = (err, files) => {
-  if(err){
-    res.send(err.message);
-  }
-}
-app.get('/', (req, res) => {
-  fs.readdir("./files",(err, files) => {
-  if(err){
-    res.status(500).send(err);
-  }else{
-    res.render("index", { files });
-  }
+app.get('/', async (req, res) => {
+  const records = await Record.find().sort({ _id: -1 });
+  res.render('index', { files: records });
 });
 
-app.get("/files/:filename/delete",(req,res)=>{
-  fs.unlink(`./files/${req.params.filename}`, (err)=>{
-    if(err) return res.status(500).send(err);
-  });
-  res.redirect(`/`);
-})
-
-});
-app.get('/files/:filename/edit', (req, res) => {
-  fs.readFile(`./files/${req.params.filename}`, 'utf8', (err, data) =>{
-        if(err){
-          res.status(500).send(err);
-        }else{
-          res.render("edit",{data,filename:req.params.filename});
-        }
-      })
-});
-app.post('/files/:filename/edit', (req, res) => {
-  fs.writeFile(`./files/${req.params.filename}`,`${req.body.prevDetails}`,(err) => {
-    if(err){
-      res.status(500).send(err);
-    }else{
-      res.redirect(`/`);
-    }
-  })
-});
 app.get('/files/create', (req, res) => {
-  res.render("create");
+  res.render('create');
 });
 
-app.post('/create', (req, res) => {
-  fs.writeFile(`./files/${req.body.date.split("/").join("")}.txt`,req.body.details,(err) => {
-    if(err){
-      res.status(500).send(err);
-    }else{
-      res.redirect(`/`);
-    }
-  })
-});
-app.get('/files/:filename', (req, res) => {
-  let filename = req.params.filename;
-  fs.readFile(`./files/${req.params.filename}`, 'utf8', (err, data) =>{
-    if(err){
-      res.status(500).send(err);
-    }else{
-      res.render("read", { data , filename});
-    }
-  })
+app.post('/create', async (req, res) => {
+  const record = new Record({ date: req.body.date, details: req.body.details });
+  await record.save();
+  res.redirect('/');
 });
 
-app.listen(3000);
+app.get('/files/:id', async (req, res) => {
+  const record = await Record.findById(req.params.id);
+  res.render('read', { data: record.details, filename: record.date });
+});
+
+app.get('/files/:id/edit', async (req, res) => {
+  const record = await Record.findById(req.params.id);
+  res.render('edit', { data: record.details, filename: record._id });
+});
+
+app.post('/files/:id/edit', async (req, res) => {
+  await Record.findByIdAndUpdate(req.params.id, { details: req.body.prevDetails });
+  res.redirect('/');
+});
+
+app.get('/files/:id/delete', async (req, res) => {
+  await Record.findByIdAndDelete(req.params.id);
+  res.redirect('/');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
